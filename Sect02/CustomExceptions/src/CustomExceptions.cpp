5556bcaf3900fac2ddbb5da181ac0c205674e067
Jt Chu <jinteckchu@gmail.com>
2025-08-31 22:18:39 -0400

    Sect02: Custom exception with noexcept

--- Sect02/CustomExceptions/src/CustomExceptions.cpp
+++ Sect02/CustomExceptions/src/CustomExceptions.cpp
@@ -13,7 +13,7 @@ using namespace std;
 
 class MyException: public exception {
 public:
-	virtual const char* what() const throw() {
+	virtual const char* what() const noexcept {
 		return "Something bad happened!";
 	}
 };
